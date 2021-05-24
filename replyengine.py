047e69ffd83355418e0f114ef389c43ac94200e2
Aadithya V <Aadithya-V@users.noreply.github.com>
2021-05-24 13:07:56 +0530

    minor formatting

--- replyengine.py
+++ replyengine.py
@@ -1,12 +1,11 @@
+import csv
 
 def get_reply(file_name, input): 
-    import csv
     with open(file_name, "r") as csv_f:
         csv_reader = csv.reader(csv_f, delimiter = ",")  
         return search(csv_reader, input) 
 
 def search(csv_reader, input) :
-    
     for line in csv_reader:
         if input == line[0]:
             line.pop(0)
